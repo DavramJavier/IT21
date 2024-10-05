56d7e5fe6fdbf869c8f494f97a5996bb40b9e2a8
DavramJavier <Harukasensei25@gmail.com>
2024-10-05 18:56:04 +0800

    Added method to display outputs via console.log and HTML on Methods.js

--- Methods.js
+++ Methods.js
@@ -16,4 +16,21 @@ class Person {
         this.age = age;
         this.occupation = occupation;
     }
+
+    //Solution 1: Display via console.log
+    displayInfo() { 
+        console.log("Name: " + this.name);
+        console.log("Age: "  + this.age);
+        console.log("occupation: " + this.occupation);
+    }
+
+    //Solution 2: Display via HTML
+    displayViaHTML() {
+        const personElement = document.getElementById('person1'); 
+      personElement.innerHTML = `
+        <strong>Name:</strong> ${this.name}<br>
+        <strong>Age:</strong> ${this.age}<br>
+        <strong>Occupation:</strong> ${this.occupation}
+        `;
+    }
 }
