700c74ed81d94ae8de0b085ff2ef6c34873a50df
DavramJavier <Harukasensei25@gmail.com>
2024-10-15 23:08:36 +0800

    Added CreateAreaChart

--- Bar Chart/chart.js
+++ Bar Chart/chart.js
@@ -39,4 +39,18 @@ class ChartCreator {
         this.createAreaChart();
     }
   
-   
+    createAreaChart() {
+        new Chart(this.areaCtx, {
+            type: 'line',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: this.chartData.data,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    fill: true,
+                    borderWidth: 2
+                }]
+            },
+          
