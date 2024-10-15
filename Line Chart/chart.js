9a075a395999c6ca02aee41965cfb7ee79d6ed50
DavramJavier <Harukasensei25@gmail.com>
2024-10-15 21:04:37 +0800

    Added fetchData

--- Line Chart/chart.js
+++ Line Chart/chart.js
@@ -4,4 +4,23 @@ class ChartCreator {
         this.chartData = null;
     }
   
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
+  
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
+  
     
